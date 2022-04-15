4f475af463c141bdc89c27bccf8251581028fcb8
Asim Shakour <shakourasim@gmail.com>
2022-04-15 11:11:35 -0500

    More clean up

--- src/App.js
+++ src/App.js
@@ -1,169 +1,99 @@
-import { useState, useEffect, useCallback } from 'react'
-import Select from 'react-select'
-
+/* eslint-disable react/prop-types */
+import { useState, useEffect, useCallback } from 'react';
+import Select from 'react-select';
 
 function App() {
-
-
-  let [dropdownList, setDropdownList] = useState([]);
   let [odropdownList, setOdropdownList] = useState([]);
   let [makeList, setMakeList] = useState([]);
 
-
-
-  // const fetchData = useCallback(() => {
-  //   fetch('./data.json')
-  //     .then(response => response.json())
-  //     .then(data => {
-  //       setCarList(data)
-  //     });
-  // }, [])
-
-  // useEffect(() => {
-  //   fetchData()
-  // }, [fetchData]);
-
   const fetchDropdown = useCallback(() => {
     fetch(
-      "https://vpic.nhtsa.dot.gov/api/vehicles/getvehiclevariablevalueslist/vehicle%20type?format=json"
+      'https://vpic.nhtsa.dot.gov/api/vehicles/getvehiclevariablevalueslist/vehicle%20type?format=json'
     )
       .then((response) => response.json())
       .then((data) => {
-        console.log(data.Results);
-        setDropdownList(data.Results);
-        let res = data.Results.map(x=>({value: x.Name, label: x.Name, ...x}));
+        let res = data.Results.map((x) => ({ value: x.Name, label: x.Name, ...x }));
         setOdropdownList(res);
       });
   }, []);
-  
-
 
   useEffect(() => {
-    fetchDropdown()
+    fetchDropdown();
   }, [fetchDropdown]);
 
-  
   const [selectedOption, setSelectedOption] = useState(null);
-  const [name, setName] = useState("");
+  const [name, setName] = useState('');
   const [checked, setChecked] = useState(false);
 
-
-  
-
   useEffect(() => {
-    console.log('Do something after counter has changed', selectedOption);
-
     function fetchMakes() {
-      console.log("fetchMakes");
-  
-      if (selectedOption &&  selectedOption.value && typeof selectedOption.value === 'string') console.error(selectedOption.value.trim().replace(" ",'%20'));
-      if (selectedOption &&  selectedOption.value && typeof selectedOption.value === 'string') {
-        console.error(selectedOption.value.trim().replace(" ",'%20'));
-        let url = "https://vpic.nhtsa.dot.gov/api/vehicles/GetMakesForVehicleType/"+selectedOption.value.trim().replace(" ",'%20')+"?format=json";
-        console.log(url)
+      if (selectedOption && selectedOption.value && typeof selectedOption.value === 'string') {
+        let url =
+          'https://vpic.nhtsa.dot.gov/api/vehicles/GetMakesForVehicleType/' +
+          selectedOption.value.trim().replace(' ', '%20') +
+          '?format=json';
+
         fetch(url)
           .then((response) => response.json())
           .then((data) => {
-            let res = data.Results.map(x=>({value: x.MakeName, label: x.MakeName, ...x}));
-            console.error(res);
+            let res = data.Results.map((x) => ({ value: x.MakeName, label: x.MakeName, ...x }));
             setMakeList(res);
           });
       }
     }
 
-    fetchMakes()
+    fetchMakes();
+  }, [selectedOption]);
 
- }, [selectedOption]);
+  const handleChangeCheck = () => {
+    setChecked(!checked);
+  };
 
- const handleChangecheck = () => {
-  setChecked(!checked);
-};
+  const [inputs, setInputs] = useState({});
 
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs((values) => ({ ...values, [name]: value }));
+  };
 
-const [inputs, setInputs] = useState({});
-
-const handleChange = (event) => {
-  const name = event.target.name;
-  const value = event.target.value;
-  setInputs(values => ({...values, [name]: value}))
-}
-
-const handleSubmit = (event) => {
-  event.preventDefault();
-  console.log(inputs);
-}
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(inputs);
+  };
 
   return (
-    <div>Vehicle Type
-    <form onSubmit={handleSubmit}>
-      <Select
-      
-        closeMenuOnSelect={true}
-        options={odropdownList}
-        defaultValue={selectedOption}
-        onChange={setSelectedOption}
+    <div>
+      Vehicle Type
+      <form onSubmit={handleSubmit} onChange={handleChange}>
+        <Select
+          closeMenuOnSelect={true}
+          options={odropdownList}
+          defaultValue={selectedOption}
+          onChange={setSelectedOption}
         />
         Make
-      <Select
-        closeMenuOnSelect={true}
-        isMulti
-        options={makeList}
-      />
-
-      <label>
-        <input
-          type="checkbox"
-          checked={checked}
-          onChange={handleChangecheck}
-        />
-        Use Year
-      </label>
-<br></br>
-      {checked && <label>Year:
-        <input
-          type="number" 
-          maxLength="4"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-      </label>}
+        <Select closeMenuOnSelect={true} isMulti options={makeList} />
+        <label>
+          <input type="checkbox" checked={checked} onChange={handleChangeCheck} />
+          Use Year
+        </label>
+        <br />
+        {checked && (
+          <label>
+            Year:
+            <input
+              type="number"
+              maxLength="4"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </label>
+        )}
       </form>
-
-      <div >
-                {body && <Table heading={heading} body={body} />}
-            </div>
-
+      {/* <div>{body && <Table heading={heading} body={body} />}</div> */}
     </div>
   );
 }
-
-let body = null;
-
-let heading = ['Make Id', 'Make Name', 'Model Id', 'Model Name'];
-
-const Table  = ({ heading, body  }) => {
-
-      return (
-          <table style={{ width: 500 }}>
-              <thead>
-                  <tr>
-                      {heading.map(head => <th>{head}</th>)}
-                  </tr>
-              </thead>
-              <tbody>
-                  {body.map(row => <TableRow row={row} />)}
-              </tbody>
-          </table>
-      );
-  }
-
-const TableRow  = ({ row }) => {
-
-      return (
-          <tr>
-              {row.map(val => <td>{val}</td>)}
-          </tr>
-      )
-  }
 
 export default App;
---
+++ src/NOTES.md
@@ -0,0 +1,33 @@
+
+// let body = null;
+
+// let heading = ['Make Id', 'Make Name', 'Model Id', 'Model Name'];
+
+// const Table = ({ heading, body }) => {
+//   return (
+//     <table style={{ width: 500 }}>
+//       <thead>
+//         <tr>
+//           {heading.map((head) => (
+//             <th>{head}</th>
+//           ))}
+//         </tr>
+//       </thead>
+//       <tbody>
+//         {body.map((row) => (
+//           <TableRow row={row} />
+//         ))}
+//       </tbody>
+//     </table>
+//   );
+// };
+
+// const TableRow = ({ row }) => {
+//   return (
+//     <tr>
+//       {row.map((val) => (
+//         <td>{val}</td>
+//       ))}
+//     </tr>
+//   );
+// };
